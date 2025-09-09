26105a3a2c1e4cc7de92951572e367175161ce03
David Lefrancq <david.lefrancq@gmail.com>
2025-09-09 18:16:38 +0200

    feat: docker dev container

--- next/next.config.ts
+++ next/next.config.ts
@@ -6,6 +6,13 @@ const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 };
 
 export default nextConfig;
