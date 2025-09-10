1be4ae668d94446b17b97ab86f11b8a29811e6d8
David Lefrancq <david.lefrancq@gmail.com>
2025-09-10 20:49:06 +0200

    fix: containers style

--- next/src/app/about/page.tsx
+++ next/src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <main className="l-container">
-      <h1>About</h1>
+      <h2 className="c-button-demo__title">About</h2>
       <p>This is the About page.</p>
     </main>
   );
--- next/src/app/contact/page.tsx
+++ next/src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function ContactPage() {
   return (
     <main className="l-container">
-      <h1>Contact</h1>
+      <h2 className="c-button-demo__title">Contact</h2>
       <p>This is the Contact page.</p>
     </main>
   );
